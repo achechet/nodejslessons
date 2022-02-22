c0d61a236e08d5b6934e69a6be9e1474c56b8342
achechet <achechet@gmail.com>
2022-02-22 14:15:14 +0300

    DudaLesson-8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    console.log("URL страницы " + req.url);
+    res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
+    res.end('Привет мир!');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Server started!")
