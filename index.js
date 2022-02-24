685d0272ebb4445220fb188c99e46612a0a2d2c1
achechet <achechet@gmail.com>
2022-02-24 16:01:32 +0300

    1

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/news/:id', function(req, res) {
     res.render('news', {newsId: req.params.id});
 });
 
-app.listen(3000, '127.0.0.1');
+app.listen(3000, '45.62.212.21');
 console.log("Server started!")
